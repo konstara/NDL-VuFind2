e3247c9258bfdba5d55c17ff05505ab53b4c27ba
konstara <konstarau@gmail.com>
2018-08-15 15:09:42 +0300

    made small fix

--- module/Finna/src/Finna/ILS/Driver/Mikromarc.php
+++ module/Finna/src/Finna/ILS/Driver/Mikromarc.php
@@ -1508,7 +1508,7 @@ class Mikromarc extends \VuFind\ILS\Driver\AbstractBase implements
             ];
             if ($patron && $this->itemHoldAllowed($item)) {
                 $entry['is_holdable'] = true;
-                $entry['level'] = 'check';
+                $entry['level'] = 'copy';
                 $entry['addLink'] = $this->config['Holds']['ShowLinkOnCopy'];
             } else {
                 $entry['is_holdable'] = false;
