afda774aadefd8f258bce200e42e2d9e03f1d95c
konstara <konstarau@gmail.com>
2018-08-23 13:46:35 +0300

    made little change to codeformat

--- themes/finna2/js/finna-image-popup.js
+++ themes/finna2/js/finna-image-popup.js
@@ -343,8 +343,7 @@ finna.imagePopup = (function finnaImagePopup() {
           $('.access-rights p').first().hide();
           $('.image-rights').hide();
           $('.media-left > .organisation-menu').hide();
-
-          if( $('.access-rights').has('.more-link') ) {
+          if ( $('.access-rights').has('.more-link') ) {
             $('.access-rights > .more-link').hide();
           }
         }
